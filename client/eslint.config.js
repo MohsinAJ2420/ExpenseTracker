f9183dbeb334e5fed690faf2112445eb358f5a46
MohsinAJ2420 <mohsinl227@gmail.com>
2025-01-30 12:48:39 +0100

    initial setup vite react

---
+++ client/eslint.config.js
@@ -0,0 +1,32 @@
+import js from "@eslint/js";
+import globals from "globals";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+
+export default [
+  {
+    ignores: ["dist"],
+  },
+  {
+    files: ["**/*.{js,jsx}"], // Ensure it only applies to JS files
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "module",
+      globals: globals.browser,
+    },
+    plugins: {
+      react,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+    rules: {
+      ...react.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+    },
+  },
+];
